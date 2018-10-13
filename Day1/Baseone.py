010ac5b725173f9b1e19d982bfbd9f843629f91d
caijc00 <923091895@qq.com>
2018-10-14 00:49:12 +0800

    Initial commit

---
+++ Day1/Baseone.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/14 0:29
+# @Author  : caijiangchun
+# @Site    : 
+# @File    : Baseone.py
+# @Software: PyCharm
+print("Hello Python")
