84181e6a495e41af60329cc8eada3e44d831ce31
caijc00 <923091895@qq.com>
2018-10-29 00:19:41 +0800

    2081029

--- Day1/Baseone.py
+++ Day1/Baseone.py
@@ -5,4 +5,5 @@
 # @Site    : 
 # @File    : Baseone.py
 # @Software: PyCharm
+
 print("Hello Python")
---
+++ Day1/learn1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/29 0:15
+# @Author  : caijiangchun
+# @Site    : 
+# @File    : learn1.py
+# @Software: PyCharm
+first_one = 1
+if first_one:
+    print("This is a true!")
